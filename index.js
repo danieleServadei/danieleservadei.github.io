3eda8adbfab0c0bd6b9bd1659a23cbbe238ea0d2
Daniele Servadei <servadei.d@gmail.com>
2019-02-18 22:54:55 +0100

    Added mp3, css, js and html main files. Index completed.

---
+++ index.js
@@ -0,0 +1,10 @@
+const promise = document.querySelector('audio').play();
+if (promise !== undefined) {
+  promise.then(_ => {
+    // spin started
+  }).catch(error => {
+    pic.addEventListener('click', function() {
+      audio.play();
+    });
+  });
+}
