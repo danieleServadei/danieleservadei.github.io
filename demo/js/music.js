40c41d64c1e925309f7d1340f4ccca1d9638a76f
Daniele Servadei <servadei.d@gmail.com>
2021-03-23 19:16:55 +0100

    logo

--- demo/js/music.js
+++ demo/js/music.js
@@ -1,16 +1,3 @@
-const promise = document.querySelector('audio').play()
-
-if (promise !== undefined) {
-  promise.then(_ => {
-    // spin started
-  }).catch(error => {
-    console.log(error)
-    heading.addEventListener('click', () => {
-      audio.play()
-    })
-  })
-}
-
-window.onbeforeunload = () => {
-  return true
-}
+logo.addEventListener('click', () => {
+  audio.play()
+})
