557d6ac3466e3e560546a1b336780cee0aa54367
Daniele Servadei <servadei.d@gmail.com>
2021-03-23 21:56:40 +0100

    pizza

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const image = document.getElementById("pic")
+const pizza = document.getElementById("pizza")
 
 pic.addEventListener('click', () => {
   if (audio.paused) {
     audio.play()
     image.src = "./napoli.jpg"
+    pizza.style.visibility = "visible"
   } else {
     audio.pause()
     image.src = "./nyan.png"
+    pizza.style.visibility = "hidden"
   }
 })
 
