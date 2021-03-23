fbcf3bc069ecf8390145b89a656f8232cee4d347
Daniele Servadei <servadei.d@gmail.com>
2021-03-23 21:47:44 +0100

    napoli

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const promise = document.querySelector('audio').play()
-if (promise !== undefined) {
-  promise.then(_ => {
-    // spin started
-  }).catch(error => {
-    pic.addEventListener('click', () => {
-      audio.play()
-    })
-  })
-}
+const image = document.getElementById("pic")
 
-window.onbeforeunload = () => {
-  return true
-}
+pic.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play()
+    image.src = "./napoli.jpg"
+  } else {
+    audio.pause()
+    image.src = "./nyan.png"
+  }
+})
+
+// window.onbeforeunload = () => {
+//   return true
+// }
